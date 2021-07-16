e64462d7f24028e72cea629c197991b286e424c8
mariam-hany <76841573+mariam-hany@users.noreply.github.com>
2021-07-16 02:15:55 +0200

    Add files via upload

--- sub_string.h
+++ sub_string.h
@@ -1,27 +1,50 @@
 #ifndef SUB_STRING_H
 #define SUB_STRING_H
-#include "string"
-#include "vector"
+
+#include <QDialog>
+#include<string>
+#include <stack>
+#include <vector>
 using namespace std;
 
+namespace Ui {
+class sub_string;
+}
 
-class sub_string
+class sub_string : public QDialog
 {
+    Q_OBJECT
+
 public:
-    sub_string();
-   // void get_line(string y);
+    explicit sub_string(QWidget *parent = nullptr,QString x="hi");
+    ~sub_string();
     void get_string (string y);
-   //   void  get_brackets(string y);
-   void  get_line(string y);
-   //void  check_brackets(string y , int num_line);
-   void  check_tags(string y, int num_line );
-   int    get_num_of_brackets(string y, int num_line );
-   void check_invaild_tag(string y, int num_line );
-  string   correct(string y,string temp, int num_line );
-   //void  divide_str(string y, int num_line );
- //  void  missing_tag(vector<string> v );
- //  void   get_vector(string y, int num_line );
-   //   int getIndex (vector<string> v,string K);
+     //   void  get_brackets(string y);
+     void  get_line(string y);
+     void  get_line1(string y);
+     //void  check_brackets(string y , int num_line);
+     void  check_tags(string y, int num_line );
+     int    get_num_of_brackets(string y, int num_line );
+     void check_invaild_tag(string y, int num_line );
+    string   correct(string y,string temp, int num_line );
+     //void  divide_str(string y, int num_line );
+   //  void  missing_tag(vector<string> v );
+   //  void   get_vector(string y, int num_line );
+     //   int getIndex (vector<string> v,string K);
+
+private slots:
+    void on_checkmissingtag_button_clicked();
+
+    void on_checkinvalidtag_button_clicked();
+
+private:
+    Ui::sub_string *ui;
+     QString fpathprivate;
+     stack<string> s;
+     stack<int> line;
+     stack<string> s_name;
+     vector<string>ms;
+     string ya;
 };
 
 #endif // SUB_STRING_H
